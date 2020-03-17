e3417156fe549aaa6d635a7d95eda4ae877258bb
Saidul Badhon <31236650+SaidulBadhon@users.noreply.github.com>
2020-03-17 21:13:04 +0600

    xD

--- src/Pages/TechnologiesWeWorkWith/Technologies.js
+++ src/Pages/TechnologiesWeWorkWith/Technologies.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "./Technologies.css";
 
-import Angular from "../../Images/.jsLogos/Angular.svg"
-import Bootstrap from "../../Images/.jsLogos/Bootstrap.svg"
-import CSS3 from "../../Images/.jsLogos/CSS3.svg"
-import HTML5 from "../../Images/.jsLogos/HTML5.png"
-import polymer from "../../Images/.jsLogos/polymer.svg"
-import ReactJS from "../../Images/.jsLogos/ReactJS.svg"
+import Angular from "../../Images/jsLogos/Angular.svg"
+import Bootstrap from "../../Images/jsLogos/Bootstrap.svg"
+import CSS3 from "../../Images/jsLogos/CSS3.svg"
+import HTML5 from "../../Images/jsLogos/HTML5.png"
+import polymer from "../../Images/jsLogos/polymer.svg"
+import ReactJS from "../../Images/jsLogos/ReactJS.svg"
 
-import Section1 from "../../Images/.jsLogos/Section1.svg"
+import Section1 from "../../Images/jsLogos/Section1.svg"
 
 
 function Technologies() {
